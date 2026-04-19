320aef194e7070401192aa32a7e355731609d7e1
priya <priya@172.28.30.220>
2026-04-19 13:35:36 +1200

    hi

--- src/cinemabookingsystem/CinemaApp.java
+++ src/cinemabookingsystem/CinemaApp.java
@@ -16,6 +16,7 @@ public class CinemaApp {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello P");
+        System.out.println("p");
     }
     
 }
